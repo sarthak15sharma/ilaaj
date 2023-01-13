993374d9b26f40573fb363d1b0fa5fee47f51744
Darsh <darsh.kejriwal@gmail.com>
2023-01-13 14:35:13 +0530

    ..

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const ejsMate =- require('ejs-mate');
+const ejsMate = require('ejs-mate');
 const path = require('path')
 
 app.engine('ejs', ejsMate);
@@ -8,10 +8,8 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({entended: true}));
+app.use(express.urlencoded({ entended: true }));
 app.use(express.json());
-
-
 
 app.listen(3000, () => {
     console.log('listening to port 3000');
