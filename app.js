15813848a48bf9752f87f7ed5f5a10f01ccdbce6
sarthak15sharma <sarthak15sharma@gmail.com>
2023-01-13 20:08:19 +0530

    views directory

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('pages/explore');
+})
 
 app.listen(3000, () => {
     console.log('listening to port 3000');
